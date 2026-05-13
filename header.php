d256857bad703bb934ee34ce2eac2dbc624284c3
macasmartina <macas.martina@es.uazuay.edu.ec>
2026-05-13 10:41:57 -0500

    cambios de estilos del header ventana emergente

--- header.php
+++ header.php
@@ -19,37 +19,52 @@
     <div class="nav-derecha">
 
         <div class="user-dropdown">
+
             <?php if (isset($_SESSION["id_cliente"])) { ?>
-                <a href="#" class="icono-usuario">
-                    <i class="fa-regular fa-user"></i>
-                </a>
-                    <div class="dropdown-content">
-                        <div class="user-info-mini">
-                            <p>Hola, <strong><?php echo $_SESSION["nombre"]; ?></strong></p>
-                        </div>
-                            <a href="mi-cuenta.php">
-                                <i class="fa-solid fa-gear"></i> Mi Cuenta
-                            </a>
-                            <a href="php/cerrar_sesion.php" class="txt-rojo">
-                                <i class="fa-solid fa-arrow-right-from-bracket"></i> Salir
-                            </a>
-                    </div>
-                <?php } else { ?>
-                    <a href="login.php" class="icono-usuario" title="Iniciar sesión">
+
+                <div class="usuario-header-mini">
+
+                    <a href="#" class="icono-usuario">
                         <i class="fa-regular fa-user"></i>
                     </a>
+
+                    <span class="texto-usuario-header">
+                        Hola, <?php echo $_SESSION["nombre"]; ?>
+                    </span>
+
+                </div>
+
+                <div class="dropdown-content">
+
+                    <a href="mi-cuenta.php">
+                        <i class="fa-solid fa-gear"></i> Mi Cuenta
+                    </a>
+
+                    <a href="php/cerrar_sesion.php" class="txt-rojo">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i> Salir
+                    </a>
+
+                </div>
+
+            <?php } else { ?>
+
+                <a href="login.php" class="icono-usuario" title="Iniciar sesión">
+                    <i class="fa-regular fa-user"></i>
+                </a>
+
             <?php } ?>
+
         </div>
 
-        <a href="carrito.php" class="botonCarrito" title="Carrito de compras">
+        <a href="carrito.php" class="botonCarrito">
             <i class="fa-solid fa-cart-shopping"></i>
             <span id="contador-carrito">0</span>
         </a>
 
-        <a href="inscripcion.php" class="btn-gym">
+        <a href="registro.php" class="btn-gym">
             Inscríbete ya
         </a>
+
     </div>
 
 </header>
-
