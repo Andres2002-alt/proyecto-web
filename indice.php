7dde2ef6e7d8ef6f27de51b53b8f570cabcf03ab
macasmartina <macas.martina@es.uazuay.edu.ec>
2026-05-13 10:08:18 -0500

    cambios del header responsive

--- indice.php
+++ indice.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <title>PowerFit Gym</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilos.css?v=700">
     <link rel="icon" type="image/png" href="imagenes/favicon.png?v=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- login.php
+++ login.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>Iniciar sesión - PowerFit Gym</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilos.css?v=700">
     <link rel="icon" type="image/png" href="imagenes/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
